8190abf2d87f221fb60d5fa89c9184daf6e914e8
jbzdarkid <jbzdarkid@gmail.com>
2023-06-11 15:32:47 -0700

    revert

--- schem/level.py
+++ schem/level.py
@@ -193,6 +193,7 @@ class DefenseLevel(Level):
             self['random-input-zones'] = {}
             self['fixed-input-zones'] = {}
             self['weapons'] = {}
+            self['terrain'] = 0
             self['max-reactors'] = 0
             self['has-starter'] = False
             self['has-assembly'] = False
